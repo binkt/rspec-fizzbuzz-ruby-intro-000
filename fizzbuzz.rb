f98bb7e078574ab14f2d4a385e69379e78a39c69
Dennis Triplo <dennistriplo@yahoo.com>
2019-02-19 21:03:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(the_num)
-  if the_num + 3 == 11
+  if the_num + 3 == 10
     puts "Fizz"
     puts "#{the_num}"
   else
