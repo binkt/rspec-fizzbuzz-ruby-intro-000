cb3e8cd06386fb082f29fe024e46de0b233490eb
Dennis Triplo <dennistriplo@yahoo.com>
2019-02-19 19:22:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(the_num)
+  if #{the_num}%3 == 0
+    puts "Fizz"
+  else
+  end
+end
