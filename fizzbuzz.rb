72683a9da6c1b774489ee652e2cbeee1d02d3ecd
Dennis Triplo <dennistriplo@yahoo.com>
2019-02-19 20:57:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(the_num)
-  if #{the_num}+3 == 0
+  if 3+3 == 0
     puts "Fizz"
-    puts "#{the_num} is the number"
   else
     puts "Poop"
   end
